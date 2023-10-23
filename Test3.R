007db3cb6b1d20c92b785e51ec8bcae7711d7db9
Michael A. Merchant <83989128+RemoteSenseiMichael@users.noreply.github.com>
2023-10-23 13:46:45 -0700

    Add files via upload

---
+++ Test3.R
@@ -0,0 +1,12 @@
+# Install the following packages if you have not already:
+install.packages('raster')
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages('data.table')
+
+# Once or if the packages are installed, then load them into R:
+library(raster)
+library (randomForest)
+library(ggplot2)
+library (data.table)
+
